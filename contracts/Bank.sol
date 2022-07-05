41cde8210fdfeba0e1578e730b7eb976b1f1c73f
mrmuffinnxz <71134901+mrmuffinnxz@users.noreply.github.com>
2022-07-05 17:17:22 +0700

    keep track of account name => account

--- contracts/Bank.sol
+++ contracts/Bank.sol
@@ -6,13 +6,15 @@ import "./Account.sol";
 
 contract Bank {
     mapping(address => Account[]) users;
+    mapping(string => Account) accounts;
     mapping(string => bool) isNameExist;
 
     function addAccount(string memory _name) public returns (string memory) {
         require(isNameExist[_name] == false, "add_account_fail_name_exist");
         isNameExist[_name] = true;
-        Account newAccount = new Account(_name);
+        Account newAccount = new Account(msg.sender, _name);
         users[msg.sender].push(newAccount);
+        accounts[_name] = newAccount;
         return "add_account_success";
     }
 
