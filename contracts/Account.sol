91ac5a1b9970721dc457433559c46dee1d313b3e
mrmuffinnxz <71134901+mrmuffinnxz@users.noreply.github.com>
2022-07-05 17:14:31 +0700

    add basic account

--- contracts/Account.sol
+++ contracts/Account.sol
@@ -3,11 +3,40 @@
 pragma solidity 0.8.15;
 
 contract Account {
+    address payable owner;
     string name;
-    uint256 balance;
 
-    constructor(string memory _name) {
+    constructor(address _owner, string memory _name) {
+        owner = payable(_owner);
         name = _name;
-        balance = 0;
+    }
+
+    function deposit() public payable returns (string memory) {
+        require(msg.value > 0, "deposit_fail_value_zero");
+        return "deposit_success";
+    }
+
+    function withdrawn(uint amount) public returns (string memory) {
+        require(owner == msg.sender, "withdrawn_fail_not_owner");
+        require(amount > 0, "withdrawn_fail_value_zero");
+        require(address(this).balance >= amount, "withdrawn_fail_balance_not_enough");
+        payable(msg.sender).transfer(amount);
+        return "withdrawn_success";
+    }
+
+    function transfer(Account receiver, uint amount) public returns (string memory) {
+        require(owner == msg.sender, "withdrawn_fail_not_owner");
+        require(amount > 0, "withdrawn_fail_value_zero");
+        require(address(this).balance >= amount, "withdrawn_fail_balance_not_enough");
+        receiver.getOwner().transfer(amount);
+        return "transfer_success";
+    }
+
+    function getOwner() public view returns (address payable) {
+        return owner;
+    }
+
+    function getBalance() public view returns (uint) {
+        return address(this).balance;
     }
 }
