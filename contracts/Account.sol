a53553c12bdf2cdd600bffda5026f534ea4feafb
mrmuffinnxz <71134901+mrmuffinnxz@users.noreply.github.com>
2022-07-05 16:35:28 +0700

    create account

--- contracts/Account.sol
+++ contracts/Account.sol
@@ -3,5 +3,11 @@
 pragma solidity 0.8.15;
 
 contract Account {
+    string name;
+    uint256 balance;
 
+    constructor(string memory _name) {
+        name = _name;
+        balance = 0;
+    }
 }
--- contracts/Bank.sol
+++ contracts/Bank.sol
@@ -2,6 +2,21 @@
 
 pragma solidity 0.8.15;
 
-contract Bank {
+import "./Account.sol";
 
+contract Bank {
+    mapping(address => Account[]) users;
+    mapping(string => bool) isNameExist;
+
+    function addAccount(string memory _name) public returns (string memory) {
+        require(isNameExist[_name] == true, "add_account_fail_name_exist");
+        isNameExist[_name] = true;
+        Account newAccount = new Account(_name);
+        users[msg.sender].push(newAccount);
+        return "add_account_success";
+    }
+
+    function getAccounts() public view returns (Account[] memory) {
+        return users[msg.sender];
+    }
 }
