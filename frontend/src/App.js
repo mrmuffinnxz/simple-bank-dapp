d1348771106f4683aa2704b93ebafe1c4797b3ed
mrmuffinnxz <71134901+mrmuffinnxz@users.noreply.github.com>
2022-07-07 23:04:48 +0700

    starting on frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,14 @@
+import React from "react";
+import Navigation from "./components/Navigation";
+import "./css/App.css";
 
 export default function App() {
     return (
         <div className="App">
-          Hello World!
+            <div className="app_container absolute_center">
+                <Navigation />
+                Hello World;
+            </div>
         </div>
     );
 }
---
+++ frontend/src/components/Navigation.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "../css/Navigation.css";
+
+export default function Navigation() {
+    return <div className="navigation-container">
+        
+    </div>;
+}
